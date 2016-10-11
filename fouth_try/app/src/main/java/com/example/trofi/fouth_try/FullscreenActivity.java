7199499f6b95601ede8fc39c6f4f6c08577a0f8b
Dasha2016 <trofimova-dasha@mail.ru>
2016-10-11 14:56:43 +0600

    game

---
+++ fouth_try/app/src/main/java/com/example/trofi/fouth_try/FullscreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.trofi.fouth_try;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+
+    public void MyClick (View view)
+    {
+        startActivity(new Intent(FullscreenActivity.this, MainActivity.class));
+
+    }
+
+
+}
